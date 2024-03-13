092d9fe2407d5a0178573be2a055990674008a62
Royce Calvin <royce20002006@yahoo.com>
2024-03-12 22:39:03 -0500

    finished

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,41 @@
 export function getAllDogs() {
-    // Your code here 
+    return fetch('/dogs')
+    
 }
 
 export function getDogNumberTwo() {
-    // Your code here 
+    return fetch('/dogs/2')
 }
 
 export function postNewDog() {
-    // Your code here 
+    let body = new URLSearchParams({name: 'Storm', age: '5'})
+    return fetch('/dogs',{
+        method: 'POST',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    body: body  
+    })
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here 
+     let body = new URLSearchParams({name: name, age: age})
+     let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body
+     };
+     return fetch('/dogs', options);
+
 }
 
 export function deleteDog(id) {
-      // Your code here 
+      let options = {
+        method: 'POST',
+        headers: {
+            'AUTH': 'ckyut5wau0000jyv5bsrud90y'
+        }
+
+      }
+      return fetch(`/dogs/${id}/delete`, options)
 }
